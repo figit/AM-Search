b8877c27ebc70d4507a492e5749e5bf9f2436496
Michael Ellan <mike.ellan@gmail.com>
2014-05-19 00:45:01 -0400

    AM Search: MQ, CSS, JS changes
    Tweaking MQ breakpoint
    Adding styling tweaks for header, mq output
    Pulling form based submit, wiring submit button listener

--- deploy/scripts/am-search.js
+++ deploy/scripts/am-search.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+	// Search
 	$('input[type="submit"]').on("click", function(){
 
 		// Clean existing results from dom
@@ -12,38 +13,31 @@ document.addEventListener('DOMContentLoaded', function () {
 		} else {
 			$(".input-error-text", ".results").hide();
 		};
-
-		// Form submit
-		$('form[name=parts-search]').submit(function(e){
-			e.preventDefault();
-
-			// Welp, no jsonp support on the provided endpoint so I'm not going to serialize and post anything.
-
-			// Fetch results
-			$.ajax({
-				url: "/scripts/data.json",
-				dataType: "json",
-				success: function(json) {
-					$(".loader", ".results").hide();
-					$.each(json.results, function(key, value) {
-						var resultItem = "<ul class='result'>";
-						resultItem += "<li><h3><span>Name:</span> <a href='"+value.Producturl+"''>"+value.Name+"</a></h3></li>";
-						resultItem += "<li><span>Price:</span> $"+value.Price+"</li>";
-						resultItem += "<li><span>Manufacturer:</span> "+value.Manufacturer+"</li>";
-						resultItem += "<li><span>SKU:</span> "+value.Sku+"</li>";
-						// Opted for non-css based ellipse to support the more link just afterwards
-						resultItem += "<li><span>Description:</span> "+value.Description.substring(0,300)+"... <a href='"+value.Producturl+"''>Click here for more information.</a></li>";
-						resultItem += "</ul>";
-						// Inject results
-						$('.results').append(resultItem);
-					});
-				},
-				error: function(e) {
-					$('.loader').hide();
-					console.log("Error: " + e.message);
-				}
-			});
-
+		
+		// Fetch results
+		$.ajax({
+			url: "scripts/data.json",
+			dataType: "json",
+			success: function(json) {
+				// Leaving loader in place for now, it's not applicable w/ current local loading
+				$(".loader", ".results").hide();
+				// Loop through results object
+				$.each(json.results, function(key, value) {
+					var resultItem = "<ul class='result'>";
+					resultItem += "<li><h3><span>Name:</span> <a href='"+value.Producturl+"''>"+value.Name+"</a></h3></li>";
+					resultItem += "<li><span>Price:</span> $"+value.Price+"</li>";
+					resultItem += "<li><span>Manufacturer:</span> "+value.Manufacturer+"</li>";
+					resultItem += "<li><span>SKU:</span> "+value.Sku+"</li>";
+					// Opted for non-css based ellipse to support the more link just afterwards
+					resultItem += "<li><span>Description:</span> "+value.Description.substring(0,300)+"... <a href='"+value.Producturl+"''>Click here for more information.</a></li>";
+					resultItem += "</ul>";
+					// Inject results
+					$('.results').append(resultItem);
+				});
+			},
+			error: function(e) {
+				$('.loader').hide();
+			}
 		});
 			
 	});
